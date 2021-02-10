8bb50bfaa0d2ab4bd16f58f8adf1404355e65c3c
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-11 05:46:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,57 +49,5 @@ def infogempa():
 		'potensi': em[5].text
 	}
 
-@app.route('api/ig/stalk', methods=['GET','POST'])
-def stalk():
-	if request.args.get('username'):
-			username = request.args.get('username').replace('@','')
-			igestalk = bs(get('https://www.mystalk.net/profile/%s' % username, headers={'User-Agent':'Mozilla/5.0 (Linux; Android 8.1.0; CPH1909) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.101 Mobile Safari/537.36'}).text, 'html.parser').find('div', class_='user-profile-area')
-			igestalk_ = igestalk.findAll('span')
-			thumb = igestalk.find('img')['src']
-			return {
-				'status': 200,
-				'Name': igestalk_[0].text.strip(),
-				'Username': igestalk_[1].text.strip(),
-				'Jumlah_Post': igestalk_[2].text.replace('\n',' ').strip(),
-				'Jumlah_Followers': igestalk_[3].text.replace('\n',' ').strip(),
-				'Jumlah_Following': igestalk_[4].text.replace('\n',' ').strip(),
-				'Biodata': igestalk.find('p').text.strip(),
-				'Profile_pic': thumb
-			}
-		except Exception as e:
-			print(e)
-			return {
-				'status': False,
-				'error': '[❗] Username salah!!'
-			}
-	else:
-		return {
-			'status': False,
-			'msg': '[!] Masukkan parameter username'
-		}
-
-@app.route('/api/chord', methods=['GET','POST'])
-def chord():
-	if request.args.get('q'):
-			q = request.args.get('q').replace(' ','+')
-			id = get('http://app.chordindonesia.com/?json=get_search_results&exclude=date,modified,attachments,comment_count,comment_status,thumbnail,thumbnail_images,author,excerpt,content,categories,tags,comments,custom_fields&search=%s' % q).json()['posts'][0]['id']
-			chord = get('http://app.chordindonesia.com/?json=get_post&id=%s' % id).json()
-			result = html_text.parse_html(chord['post']['content']).text_content()
-			return {
-				'status': 200,
-				'result': result
-                               }
-		except Exception as e:
-			print(e)
-			return {
-				'status': False,
-				'error': '[❗] Maaf chord yang anda cari tidak dapat saya temukan'
-                               }
-	else:
-		return {
-			'status': False,
-			'msg': '[!] Masukkan parameter q'
-		}
-
 if __name__ == '__main__':
     app.run()
