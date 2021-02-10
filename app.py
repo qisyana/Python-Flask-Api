b4c1e51e8fcdeb57b03557c8316f19cb2797a163
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-10 21:03:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,60 +8,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     a = {
-    'Contoh-Penggunaan':{'lirik': 'sdsd', 'facebook-downloader':'sadsd', 'nulis-bot':'sdsdsd', 'instagram-downloader': 's'}
+    'Contoh-Penggunaan':{'lirik': 'api/lirik?search=alone', 'random-quotes':'api/random/quotes', 'info-gempa':'api/infogempa'}
     }
     return a
 
-@app.route('/bot', methods=['GET'])
-def fb():
-    url = request.args.get('url')
-    req = requests.get(url)
-    sd = re.search('sd_src:"(.+?)"', req.text).group(1)
-    hd = re.search('hd_src:"(.+?)"', req.text).group(1)
-    js = {
-    "results-hd": hd,
-    "results-sd": sd
-    }
-    return js
-
-@app.route('/nulis', methods=['GET', 'POST'])
-def tulis():
-    from nulis import tulis
-    text = request.args.get('text')
-    tulis=tulis(text)
-    for i in tulis.tulis():
-        i.save('gambar.jpg')
-        image = open('gambar.jpg', 'rb')
-        image_read = image.read()
-        image_64_encode = base64.encodebytes(image_read)
-        url = 'https://api.imgbb.com/1/upload'
-        par = {
-         'key':'bfb879871c22df49a0694b4fb9ef6f9d',
-         'image':image_64_encode,
-         'name':'nulis',
-         'expiration': 60
-         }
-        headers = {
-         'Accept': 'application/json'
-         }
-        req = requests.post(url,data=par, headers=headers)
-        p = req.json()['data']['display_url']
-        js = {
-         "results":p
-         }
-        return js
-
-@app.route('/ig', methods=['GET'])
-def ig():
-    url = request.args.get('video_id')
-    req = requests.get('https://instagram.com/p/'+url+'?__a=1')
-    jss = req.json()['graphql']['shortcode_media']['video_url']
-    js = {
-    "results":jss
-    }
-    return js
-
-@app.route('/lirik')
+@app.route('/api/lirik')
 def lirik():
     par= request.args.get('search')
     from lirik import search
@@ -71,7 +22,7 @@ def lirik():
     }
     return b
 
-@app.route('/v1/randomquotes', methods=['GET','POST'])
+@app.route('/api/random/quotes', methods=['GET','POST'])
 def quotes():
 	quotes_file = json.loads(open('quotes.json').read())
 	result = random.choice(quotes_file)
@@ -80,6 +31,22 @@ def quotes():
 		'status': 200,
 		'author': result['author'],
 		'quotes': result['quotes']
+	}
+
+@app.route('/api/infogempa', methods=['GET','POST'])
+def infogempa():
+	be = bs(get('https://www.bmkg.go.id/').text, 'html.parser').find('div', class_="col-md-4 md-margin-bottom-10")
+	em = be.findAll('li')
+	img = be.find('a')['href']
+	return {
+		'status': 200,
+		'map': img,
+		'waktu': em[0].text,
+		'magnitude': em[1].text,
+		'kedalaman': em[2].text,
+		'koordinat': em[3].text,
+		'lokasi': em[4].text,
+		'potensi': em[5].text
 	}
 
 if __name__ == '__main__':
