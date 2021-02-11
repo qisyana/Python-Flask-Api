a6bdb31d5bb2c314db3a5dbfaa091ce1f7fcea0e
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-11 20:26:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     a = {
-    'Contoh-Penggunaan':{'lirik': 'api/lirik?search=alone', 'random-quotes':'api/random/quotes', 'info-gempa':'api/infogempa', 'stalk-ig':'api/ig/stalk?username=ekooju'}
+    'Contoh-Penggunaan':{'search-lirik': 'api/lirik?search=alone', 'random-quotes':'api/random/quotes', 'info-gempa':'api/infogempa', 'stalk-ig':'api/ig/stalk?username=ekooju'}
     }
     return a
 
@@ -72,6 +72,36 @@ def chord():
 		return {
 			'status': False,
 			'msg': '[!] Masukkan parameter q'
+		}
+
+@app.route('/api/ig/stalk', methods=['GET','POST'])
+def stalk():
+	if request.args.get('username'):
+		try:
+			username = request.args.get('username').replace('@','')
+			igestalk = bs(get('https://www.mystalk.net/profile/%s' % username, headers={'User-Agent':'Mozilla/5.0 (Linux; Android 8.1.0; CPH1909) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.101 Mobile Safari/537.36'}).text, 'html.parser').find('div', class_='user-profile-area')
+			igestalk_ = igestalk.findAll('span')
+			thumb = igestalk.find('img')['src']
+			return {
+				'status': 200,
+				'Name': igestalk_[0].text.strip(),
+				'Username': igestalk_[1].text.strip(),
+				'Jumlah_Post': igestalk_[2].text.replace('\n',' ').strip(),
+				'Jumlah_Followers': igestalk_[3].text.replace('\n',' ').strip(),
+				'Jumlah_Following': igestalk_[4].text.replace('\n',' ').strip(),
+				'Biodata': igestalk.find('p').text.strip(),
+				'Profile_pic': thumb
+			}
+		except Exception as e:
+			print(e)
+			return {
+				'status': False,
+				'error': '[❗] Username salah!!'
+			}
+	else:
+		return {
+			'status': False,
+			'msg': '[!] Masukkan parameter username'
 		}
 
 if __name__ == '__main__':
