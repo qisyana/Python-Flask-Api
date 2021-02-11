347e1cb4da93d955b5881f851a81bd200b7f0962
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-11 21:44:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def home():
 
 @app.route('/api/lirik')
 def lirik():
-    par= request.args.get('search')
+    url= request.args.get('search')
     from lirik import search
-    a = search(par)
+    a = search(url)
     b = {
     'results': a.result()
     }
@@ -25,8 +25,8 @@ def lirik():
 
 @app.route('/api/random/quotes', methods=['GET','POST'])
 def quotes():
-	quotes_file = json.loads(open('quotes.json').read())
-	result = random.choice(quotes_file)
+	quott = json.loads(open('quotes.json').read())
+	result = random.choice(quott)
 	print(result)
 	return {
 		'status': 200,
@@ -36,8 +36,8 @@ def quotes():
 
 @app.route('/api/random/asmaulhusna', methods=['GET','POST'])
 def quotes():
-	quotes_file = json.loads(open('asmaul.json').read())
-	result = random.choice(quotes_file)
+	asmaul = json.loads(open('asmaul.json').read())
+	result = random.choice(asmaul)
 	print(result)
 	return {
 		'status': 200,
@@ -50,26 +50,26 @@ def quotes():
 
 @app.route('/api/infogempa', methods=['GET','POST'])
 def infogempa():
-	be = bs(get('https://www.bmkg.go.id/').text, 'html.parser').find('div', class_="col-md-4 md-margin-bottom-10")
-	em = be.findAll('li')
-	img = be.find('a')['href']
+	hehe = bs(get('https://www.bmkg.go.id/').text, 'html.parser').find('div', class_="col-md-4 md-margin-bottom-10")
+	anjay = hehe.findAll('li')
+	gambar = hehe.find('a')['href']
 	return {
 		'status': 200,
-		'map': img,
-		'waktu': em[0].text,
-		'magnitude': em[1].text,
-		'kedalaman': em[2].text,
-		'koordinat': em[3].text,
-		'lokasi': em[4].text,
-		'potensi': em[5].text
+		'map': gambar,
+		'waktu': anjay[0].text,
+		'magnitude': anjay[1].text,
+		'kedalaman': anjay[2].text,
+		'koordinat': anjay[3].text,
+		'lokasi': anjay[4].text,
+		'potensi': anjay[5].text
 	}
 
 @app.route('/api/chord', methods=['GET','POST'])
 def chord():
-	if request.args.get('q'):
+	if request.args.get('lagu'):
 		try:
-			q = request.args.get('q').replace(' ','+')
-			id = get('http://app.chordindonesia.com/?json=get_search_results&exclude=date,modified,attachments,comment_count,comment_status,thumbnail,thumbnail_images,author,excerpt,content,categories,tags,comments,custom_fields&search=%s' % q).json()['posts'][0]['id']
+			lagu = request.args.get('lagu').replace(' ','+')
+			id = get('http://app.chordindonesia.com/?json=get_search_results&exclude=date,modified,attachments,comment_count,comment_status,thumbnail,thumbnail_images,author,excerpt,content,categories,tags,comments,custom_fields&search=%s' % lagu).json()['posts'][0]['id']
 			chord = get('http://app.chordindonesia.com/?json=get_post&id=%s' % id).json()
 			result = html_text.parse_html(chord['post']['content']).text_content()
 			return {
@@ -80,42 +80,12 @@ def chord():
 			print(e)
 			return {
 				'status': False,
-				'error': '[❗] Maaf chord yang anda cari tidak dapat saya temukan!'
+				'error': '[❗] Maaf chord lagu yang anda cari tidak ditemukan!'
 			}
 	else:
 		return {
 			'status': False,
-			'msg': '[!] Masukkan parameter q'
-		}
-
-@app.route('/api/ig/stalk', methods=['GET','POST'])
-def stalk():
-	if request.args.get('username'):
-		try:
-			username = request.args.get('username').replace('@','')
-			igestalk = bs(get('https://www.mystalk.net/profile/%s' % username, headers={'User-Agent':'Mozilla/5.0 (Linux; Android 8.1.0; CPH1909) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.101 Mobile Safari/537.36'}).text, 'html.parser').find('div', class_='user-profile-area')
-			igestalk_ = igestalk.findAll('span')
-			thumb = igestalk.find('img')['src']
-			return {
-				'status': 200,
-				'Name': igestalk_[0].text.strip(),
-				'Username': igestalk_[1].text.strip(),
-				'Jumlah_Post': igestalk_[2].text.replace('\n',' ').strip(),
-				'Jumlah_Followers': igestalk_[3].text.replace('\n',' ').strip(),
-				'Jumlah_Following': igestalk_[4].text.replace('\n',' ').strip(),
-				'Biodata': igestalk.find('p').text.strip(),
-				'Profile_pic': thumb
-			}
-		except Exception as e:
-			print(e)
-			return {
-				'status': False,
-				'error': '[❗] Username salah!!'
-			}
-	else:
-		return {
-			'status': False,
-			'msg': '[!] Masukkan parameter username'
+			'msg': '[!] Masukkan parameter lagu'
 		}
 
 if __name__ == '__main__':
