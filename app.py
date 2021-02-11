0e21e8aa56a109a7c699ea949f962ff3c16ee67f
Zhirrr <75620279+Zhirrr@users.noreply.github.com>
2021-02-11 20:14:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from requests import get, post
 from bs4 import BeautifulSoup as bs
-import json, base64, random
+import json, base64, random, html_text
 
 app = Flask(__name__)
 
@@ -48,6 +48,30 @@ def infogempa():
 		'lokasi': em[4].text,
 		'potensi': em[5].text
 	}
+
+@app.route('/api/chord', methods=['GET','POST'])
+def chord():
+	if request.args.get('q'):
+		try:
+			q = request.args.get('q').replace(' ','+')
+			id = get('http://app.chordindonesia.com/?json=get_search_results&exclude=date,modified,attachments,comment_count,comment_status,thumbnail,thumbnail_images,author,excerpt,content,categories,tags,comments,custom_fields&search=%s' % q).json()['posts'][0]['id']
+			chord = get('http://app.chordindonesia.com/?json=get_post&id=%s' % id).json()
+			result = html_text.parse_html(chord['post']['content']).text_content()
+			return {
+				'status': 200,
+				'result': result
+			}
+		except Exception as e:
+			print(e)
+			return {
+				'status': False,
+				'error': '[❗] Maaf chord yang anda cari tidak dapat saya temukan!'
+			}
+	else:
+		return {
+			'status': False,
+			'msg': '[!] Masukkan parameter q'
+		}
 
 if __name__ == '__main__':
     app.run()
